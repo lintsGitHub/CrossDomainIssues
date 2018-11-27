339b2cda3d37aceafc210750130bc01401f42bd6
LLS <2145604059@qq.com>
2018-11-27 14:17:16 +0800

    跨域解决方案

--- SpringMVC-CROS/CROS/src/main/java/org/lint/cros/CROSController.java
+++ SpringMVC-CROS/CROS/src/main/java/org/lint/cros/CROSController.java
@@ -4,14 +4,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping
 public class CROSController {
 
-    @CrossOrigin
+    //@CrossOrigin
     @RequestMapping("/hello")
     public String seyHello(){
         return "成功返回数据";
     }
 
+
+    @RequestMapping("/hi")
+    public String seyHi(){
+        return "成功返回Hi";
+    }
 }
