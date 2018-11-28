ddffa41284d692d551d50ecfed6998f387bfb432
LLS <2145604059@qq.com>
2018-11-28 11:43:49 +0800

    更新--对SpringMVC中的CROS进一步了解

--- SpringMVC-CROS/CROS/src/main/java/org/lint/cros/CROSController.java
+++ SpringMVC-CROS/CROS/src/main/java/org/lint/cros/CROSController.java
@@ -1,21 +1,22 @@
 package org.lint.cros;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin
-@RestController
+@CrossOrigin
+
+@Controller
 @RequestMapping
 public class CROSController{
 
-    //@CrossOrigin
+    @CrossOrigin
+    @ResponseBody
     @RequestMapping("/hello")
     public String seyHello(){
         return "成功返回数据";
     }
 
-
+    @ResponseBody
     @RequestMapping("/hi")
     public String seyHi(){
         return "成功返回Hi";
